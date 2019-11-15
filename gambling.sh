1c24e1488fd9db3d96d4c4183514031387ca3348
GaikwadRavi03 <gaikwadr576@gmail.com>
2019-11-15 17:20:21 -0500

    Add: Luckey and Unlucky Day Found

--- gambling.sh
+++ gambling.sh
@@ -10,6 +10,7 @@ TotalStake=0
 VALUE=50
 
 dailyBetAmount=$STAKE
+declare -A Dictionary
 
 function limit () {
 	Maxlimit=$(($dailyBetAmount+$dailyBetAmount*50/100))
@@ -44,10 +45,10 @@ function betting() {
 	if [ $dailyBetAmount -eq $Minlimit ]
 	then
 		TotalStake=$(($TotalStake-$VALUE))	
-		echo "TotalStake : $TotalStake"
+		Dictionary[$counter]=$TotalStake
 	else
 		TotalStake=$(($TotalStake+$VALUE))
-		echo "TotalStake : $TotalStake"
+		Dictionary[$counter]=$TotalStake
 	fi
   	dailyBetAmount=100
   done
@@ -56,3 +57,23 @@ function betting() {
 limit
 betting
 echo "now your Stakes : $TotalStake"
+
+profitValue=$(printf "%s\n" ${Dictionary[@]} | sort -n | tail -1 )
+lossValue=$(printf "%s\n" ${Dictionary[@]} | sort -n | head -1 )
+
+echo profitValue: $profitValue
+echo lossValue :$lossValue
+
+echo ${!Dictionary[@]} : ${Dictionary[@]}
+for key in ${!Dictionary[@]}
+do
+	if [ ${Dictionary[$key]} -eq $profitValue ]
+	then
+		echo "lucky day :$key"
+	fi
+
+	if [ ${Dictionary[$key]} -eq $lossValue ]
+	then
+		echo "unlucky day :$key"
+	fi
+done
